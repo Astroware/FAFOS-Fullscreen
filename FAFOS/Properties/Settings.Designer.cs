4f5db821b782f7efaf63a4c5a9ac50a0b248ba10
Trevor <tjcrawford15@gmail.com>
2014-04-02 20:29:36 -0400

    Fixed File Duplication Error
    Had to modify the .csproj compilation instructions to get rid of
    duplicate files

--- FAFOS/Properties/Settings.Designer.cs
+++ FAFOS/Properties/Settings.Designer.cs
@@ -50,8 +50,7 @@ namespace FAFOS.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SEDGESQL;AttachDbFilename=C:\\SEdge2013\\SystemDatabase\\Startup+Datab" +
-            "ase\\FAFOS.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SEDGESQL;AttachDbFilename=C:\\SEdge2013\\SystemDatabase\\FAFOS.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True")]
         public string FAFOS {
             get {
                 return ((string)(this["FAFOS"]));
@@ -61,8 +60,7 @@ namespace FAFOS.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SEDGESQL;AttachDbFilename=C:\\SEdge2013\\SystemDatabase\\Startup+Datab" +
-            "ase\\FAFOS_HQ.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SEDGESQL;AttachDbFilename=C:\\SEdge2013\\SystemDatabase\\FAFOS_HQ.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True")]
         public string HQ {
             get {
                 return ((string)(this["HQ"]));
